6592f1473375ad3102b5896e9f1e73f1ec5b1274
patomdev <patryk.omiotek@gmail.com>
2017-02-27 04:50:42 +0100

    updated swagger definitions

--- app/endpoints/sites.py
+++ app/endpoints/sites.py
@@ -18,8 +18,6 @@ FILTER = {
     'title': fields.Str()
 }
 
-path = os.path.abspath(os.path.dirname(__file__))
-
 def normalize(siteDoc):
     return {
         'id': str(ObjectId(siteDoc['_id'])),
@@ -32,13 +30,45 @@ def normalize(siteDoc):
 @blueprint.route('<string:id>')
 @blueprint.route('', defaults={'id': 0})
 def get(id):
+    """
+    Returns one page
+    ---
+    description: Returns site by ID
+    summary: Find site by ID
+    operationId: get
+    produces:
+    - application/json
+    - text/html
+    tags:
+      - sites
+    parameters:
+      - name: id
+        in: path
+        description: Id of site to use
+        required: true
+        type: string
+    responses:
+      200:
+        description: Returns one page
+    """
     db = get_db()
     siteDoc = db['sites'].find_one({"_id": ObjectId(id)})
     site = normalize(siteDoc)
     return dumps(site)
 
-@blueprint.route('/')
+@blueprint.route('')
 def get_list():
+    """
+    Get all sites
+    ---
+    description: Returns list of sites
+    summary: Returns a list of sites
+    tags:
+      - sites
+    responses:
+      200:
+        description: Returns a list of sites
+    """
     db = get_db()
     sites = []
     for doc in db['sites'].find({}).sort([{'crawled', pymongo.DESCENDING}]):
@@ -49,17 +79,78 @@ def get_list():
 @blueprint.route('', methods=['POST'])
 @flaskparser.use_kwargs(FILTER)
 def post(url, title):
+    """
+    Creates new site
+    ---
+    summary: Creates new site
+    tags:
+      - sites
+    definitions:
+      - schema:
+          id: Site
+          properties:
+            id:
+             type: string
+             description: the site's name
+    parameters:
+      - in: body
+        name: body
+        schema:
+          required:
+            - url
+            - title
+          properties:
+            url:
+              type: string
+              description: crawled url
+            title:
+              type: string
+              description: title of page
+    responses:
+      201:
+        description: Data of site
+
+    """
     db = get_db()
     db['sites'].insert_one({'url': url, 'title': title})
 
     return jsonify({'status': 'ok'}), 201
-post.__doc__ = """
-Saves new site
-
-swagger_from_file: %s/sites.yaml
-""" % path
 
 @blueprint.route('<int:id>', methods=['PUT'])
 @flaskparser.use_kwargs(FILTER)
 def put(id, url, title):
+    """
+    Updates site
+    ---
+    summary: Updates site
+    tags:
+      - sites
+    definitions:
+      - schema:
+          id: Site
+          properties:
+            id:
+             type: string
+             description: the site's name
+    parameters:
+      - in: body
+        name: body
+        schema:
+          id: Site
+          required:
+            - url
+            - title
+          properties:
+            url:
+              type: string
+              description: crawled url
+            title:
+              type: string
+              description: title of page
+    responses:
+      200:
+        description: Updated data of site
+
+    """
     return jsonify({'id': id, 'url': url, 'title': title})
+
